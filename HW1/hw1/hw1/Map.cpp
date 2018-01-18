750c22f5adbcc8242ed61a9bf23d4fd1415f2742
jeffding412 <dingzhengfuaa@gmail.com>
2018-01-17 21:29:37 -0800

    HW1 Map Constant Function
    Seperated Files and added const to functions that don't change the map

--- HW1/hw1/hw1/Map.cpp
+++ HW1/hw1/hw1/Map.cpp
@@ -1,16 +1,9 @@
 //
-//  main.cpp
+//  Map.cpp
 //  hw1
 //
 //  Created by Jeffrey Ding on 1/17/18.
 //  Copyright © 2018 Jeffrey Ding. All rights reserved.
 //
 
-#include <iostream>
 #include "Map.h"
-using namespace std;
-
-int main()
-{
-    cout << "Hello World" << endl;
-}
---
+++ HW1/hw1/hw1/Map.h
@@ -0,0 +1,71 @@
+//
+//  Map.h
+//  hw1
+//
+//  Created by Jeffrey Ding on 1/17/18.
+//  Copyright © 2018 Jeffrey Ding. All rights reserved.
+//
+
+#ifndef Map_h
+#define Map_h
+
+#include <string>
+
+class Map
+{
+public:
+    Map();         // Create an empty map (i.e., one with no key/value pairs)
+    
+    bool empty() const;  // Return true if the map is empty, otherwise false.
+    
+    int size() const;    // Return the number of key/value pairs in the map.
+    
+    bool insert(const std::string& key, const double& value);
+    // If key is not equal to any key currently in the map, and if the
+    // key/value pair can be added to the map, then do so and return true.
+    // Otherwise, make no change to the map and return false (indicating
+    // that either the key is already in the map, or the map has a fixed
+    // capacity and is full).
+    
+    bool update(const std::string& key, const double& value);
+    // If key is equal to a key currently in the map, then make that key no
+    // longer map to the value it currently maps to, but instead map to
+    // the value of the second parameter; return true in this case.
+    // Otherwise, make no change to the map and return false.
+    
+    bool insertOrUpdate(const std::string& key, const double& value);
+    // If key is equal to a key currently in the map, then make that key no
+    // longer map to the value it currently maps to, but instead map to
+    // the value of the second parameter; return true in this case.
+    // If key is not equal to any key currently in the map and if the
+    // key/value pair can be added to the map, then do so and return true.
+    // Otherwise, make no change to the map and return false (indicating
+    // that the key is not already in the map and the map has a fixed
+    // capacity and is full).
+    
+    bool erase(const std::string& key);
+    // If key is equal to a key currently in the map, remove the key/value
+    // pair with that key from the map and return true.  Otherwise, make
+    // no change to the map and return false.
+    
+    bool contains(const std::string& key) const;
+    // Return true if key is equal to a key currently in the map, otherwise
+    // false.
+    
+    bool get(const std::string& key, double& value) const;
+    // If key is equal to a key currently in the map, set value to the
+    // value in the map that that key maps to, and return true.  Otherwise,
+    // make no change to the value parameter of this function and return
+    // false.
+    
+    bool get(int i, std::string& key, double& value) const;
+    // If 0 <= i < size(), copy into the key and value parameters the
+    // key and value of one of the key/value pairs in the map and return
+    // true.  Otherwise, leave the key and value parameters unchanged and
+    // return false.  (See below for details about this function.)
+    
+    void swap(Map& other);
+    // Exchange the contents of this map with the other one.
+};
+
+#endif /* Map_h */
