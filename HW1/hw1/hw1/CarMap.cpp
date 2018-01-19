f3853a38cf6e3b2d29f75de084e9ca2b46642f1d
jeffding412 <dingzhengfuaa@gmail.com>
2018-01-19 00:11:56 -0800

    Created CarMap files
    Created CarMap files and finished CarMap.h

---
+++ HW1/hw1/hw1/CarMap.cpp
@@ -0,0 +1,10 @@
+//
+//  CarMap.cpp
+//  hw1
+//
+//  Created by Jeffrey Ding on 1/18/18.
+//  Copyright © 2018 Jeffrey Ding. All rights reserved.
+//
+
+#include "CarMap.h"
+
--- HW1/hw1/hw1/main.cpp
+++ HW1/hw1/hw1/main.cpp
@@ -13,15 +13,6 @@ using namespace std;
 
 int main()
 {
-    string name = "None";
-    double value = 0;
-    Map m1;
-    m1.insert("Fred", 2.956);
-    Map m2;
-    m2.insert("Ethel", 3.538);
-    m2.insert("Lucy", 2.956);
-    m1.swap(m2);
+    Map m;  // maps ints to strings
     
-    m2.get(1, name, value);
-    cout << name << " " << value << endl;
 }
