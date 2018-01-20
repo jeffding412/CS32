a4ca0bbbfe1dc5613d5180fa1d316d8a1c2a88a4
jeffding412 <dingzhengfuaa@gmail.com>
2018-01-19 21:49:16 -0800

    Start newMap class
    Created newMap class header and cpp file to create a dynamic map

---
+++ HW1/hw1/hw1/newMap.cpp
@@ -0,0 +1,9 @@
+//
+//  newMap.cpp
+//  hw1
+//
+//  Created by Jeffrey Ding on 1/19/18.
+//  Copyright © 2018 Jeffrey Ding. All rights reserved.
+//
+
+#include "newMap.h"
---
+++ HW1/hw1/hw1/newMap.h
@@ -0,0 +1,14 @@
+//
+//  newMap.h
+//  hw1
+//
+//  Created by Jeffrey Ding on 1/19/18.
+//  Copyright © 2018 Jeffrey Ding. All rights reserved.
+//
+
+#ifndef newMap_h
+#define newMap_h
+
+
+
+#endif /* newMap_h */
