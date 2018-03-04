f44285aa2bed7cd6a1e9f7510c098581a6fc4fb1
jeffding412 <dingzhengfuaa@gmail.com>
2018-03-03 20:26:58 -0800

    HW4
    Finished HW4

---
+++ HW4/hw4problem3/DerivedData/hw4problem3/Build/Intermediates.noindex/hw4problem3.build/Debug/hw4problem3.build/Objects-normal/x86_64/list.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jeffreyding/Desktop/Computer\ Science\ Projects/cs32/HW4/hw4problem3/hw4problem3/list.cpp
---
+++ HW4/hw4problem6/DerivedData/hw4problem6/Build/Intermediates.noindex/hw4problem6.build/Debug/hw4problem6.build/Objects-normal/x86_64/sorts.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jeffreyding/Desktop/Computer\ Science\ Projects/cs32/HW4/hw4problem6/hw4problem6/sorts.cpp
